667fc6e130eda7ac9da29c8c3ec8ef7e1478998a
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 15:35:43 +0200

    add jsnack4  script

---
+++ jsnack4/script4.js
@@ -0,0 +1,17 @@
+let invited = ['Paolo', 'Peppino', 'Giovanni', 'Giacomino', 'Lucia', 'Alessia', 'Alfredo', 'Piersilvio' ];
+
+let userName = prompt('inserisci il tuo nome:');
+
+
+
+
+
+let invitedList = invited.length;
+
+if(userName.includes(userName)){
+
+        console.log('tu  puoi passare');
+    }else {
+        console.log("lei non puo' entrare!");
+    };
+
