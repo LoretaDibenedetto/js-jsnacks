8ec4d85ca5d58ab1d2856f3c519f800b20ade5ff
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 16:49:14 +0200

    add cicle for and if with multiplication table

--- jsnack7/script7.js
+++ jsnack7/script7.js
@@ -3,3 +3,13 @@
 
 /*scrivi un programma che stampa la tabellina del 2 fino al numero 1000 modificate poi il programma in modo che venga chiesto all utente il numero massimo consentito e stamparla fino al numero consentito */
 
+for(i= 2; i< 1000; i++){
+
+if(i % 2 == 0){
+
+    console.log(i);
+}
+
+
+
+};
