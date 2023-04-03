ef56b6a6657b3e084dc3ad5e77828d6ae577fb88
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 17:03:45 +0200

    fix script7

--- jsnack7/script7.js
+++ jsnack7/script7.js
@@ -1,6 +1,10 @@
 //jsnack 7
 let userNumber = parseInt(prompt('inserisci un numero'));
 
+
+if(userNumber < 2){
+    alert('metti un numero da almeno 2 in poi');
+};
 /*scrivi un programma che stampa la tabellina del 2 fino al numero 1000 modificate poi il programma in modo che venga chiesto all utente il numero massimo consentito e stamparla fino al numero consentito */
 
 /*for(i= 2; i< 1000; i++){
