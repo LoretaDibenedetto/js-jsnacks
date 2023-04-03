dfec8f12976cec8998021d8dd545299335880963
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 17:00:34 +0200

    add jsnack 7

--- jsnack7/script7.js
+++ jsnack7/script7.js
@@ -1,15 +1,26 @@
 //jsnack 7
-
+let userNumber = parseInt(prompt('inserisci un numero'));
 
 /*scrivi un programma che stampa la tabellina del 2 fino al numero 1000 modificate poi il programma in modo che venga chiesto all utente il numero massimo consentito e stamparla fino al numero consentito */
 
-for(i= 2; i< 1000; i++){
+/*for(i= 2; i< 1000; i++){
 
 if(i % 2 == 0){
 
-    console.log(i);
+    console.log( i);
 }
 
 
+
+};*/
+
+for(i = 2; i <= userNumber;i++){
+
+    
+    if(i % 2 == 0){
+
+        console.log(i);
+    }
+    
 
 };
