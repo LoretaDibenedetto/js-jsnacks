7205f134af5378a59db4231d5b682a61dd33ad61
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 16:45:27 +0200

    add new folder

---
+++ jsnack7/script7.js
@@ -0,0 +1,5 @@
+//jsnack 7
+
+
+/*scrivi un programma che stampa la tabellina del 2 fino al numero 1000 modificate poi il programma in modo che venga chiesto all utente il numero massimo consentito e stamparla fino al numero consentito */
+
