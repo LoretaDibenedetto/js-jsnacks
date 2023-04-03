a1461eb780c8579f62406f88ebaa3b16b8cc4e7d
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 17:17:03 +0200

    fix indentation

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -5,11 +5,17 @@
 let numberOne = prompt('inserisci il primo numero:');
 let numberTwo = prompt('inserisci il secondo numero:');
 
+
+
+let isaNotaNumber1 = numberOne === isNaN(numberOne);
+let isaNotaNumber2 = numberTwo === isNaN(numberTwo);
+
+if (!isaNotaNumber1 && !isaNotaNumber2){
+
 numberOne = parseInt(numberOne);
 numberTwo = parseInt(numberTwo);
 
-
-
+};
 
 if(numberOne < numberTwo){
     console.log("il numero maggiore e'" + numberTwo);
@@ -18,7 +24,7 @@ if(numberOne < numberTwo){
 
 }else if(numberOne == numberTwo){
     console.log('il numeri sono uguali');
-}
+};
 
 
 
