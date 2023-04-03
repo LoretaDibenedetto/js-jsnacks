3b45f9f8b5fc6b0a9a5181f4d440e2523c3430c8
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 16:23:54 +0200

    add jsnack 6 folder

---
+++ jsnack6/script6.js
@@ -0,0 +1,5 @@
+//jsnack 6
+
+/*fate generare un numero random da 0 a 10 al computer e chiedete all'utente di inserire il suo numero se il numero scelto dall'utente e' uguale all'numero del computer informate l'utente che ha vinto altrimenti ha perso */
+
+
