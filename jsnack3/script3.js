c4f954bde04b34a592bd7df10a09988ed5af2273
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 15:09:50 +0200

    add for cicle and console.log

--- jsnack3/script3.js
+++ jsnack3/script3.js
@@ -1,1 +1,20 @@
+
+//dichiaro una variabile con array vuoto
 let userNumber = [];
+
+//normale ciclo for che itera da 1 a 10
+for (let i = 0; i< 10; i++){
+
+    //per ogni iterazione mi fara comparire un prompt
+  let number = prompt('inserisci un numero:');
+  //rendo il numero stringa un numero 
+  number = parseInt(number);
+
+  //aggiungo numeri inseriti all'array
+  userNumber.push(number);
+
+
+}
+//stampo in console
+console.log('i numeri inseriti sono: '  +' '+ userNumber);
+
