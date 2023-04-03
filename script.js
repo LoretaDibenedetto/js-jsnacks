03f09f6e6dbe722acea5d7a5286890d38bab4153
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-03 14:16:50 +0200

    init layout and init first jsnack

---
+++ script.js
@@ -0,0 +1,19 @@
+//jsnack 1
+
+let numberOne = prompt('inserisci il primo numero:');
+let numberTwo = prompt('inserisci il secondo numero:');
+
+numberOne = parseInt(numberOne);
+numberTwo = parseInt(numberTwo);
+
+
+
+
+if(numberOne < numberTwo){
+    console.log("il numero maggiore e'" + numberTwo);
+}else if(numberOne > numberTwo){
+    console.log("il numero maggiore e'" + numberOne);
+
+}else if(numberOne == numberTwo){
+    console.log('il numeri sono uguali');
+}
